c167f7a4a77f712b3e28206f68ec3fb33850e385
César Alfredo Sánchez Cen <35706134+CesarASC@users.noreply.github.com>
2018-02-11 16:13:32 -0500

    Archivos js agregados

---
+++ proyecto1/js/login.js
@@ -0,0 +1,20 @@
+function validarEmail(elemento){
+    if(elemento.value!==''){
+        var data=elemento.value;
+        var exp=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(!exp.test(data)){
+            elemento.className='error';
+        }else{
+            elemento.className='input';
+        }
+    }
+}
+function validar(){
+    var user=true;
+    var exp=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(!exp.test(document.getElementById("usuario").value)){
+        user=false;
+        alert("El usuario debe de ser un correo");
+    }
+    return user;
+}
